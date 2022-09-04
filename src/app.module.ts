00aa15c6dcab3699853e9922daa2e212eba69d27
이혜성 <main@hyeseong.local>
2022-09-05 06:38:45 +0900

    typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    autoSchemaFile: true,
-  }),
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'postgres',
+      password: '123',
+      database: 'ubereats',
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
     RestaurantsModule,
   ],
   controllers: [],
