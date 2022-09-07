0fec901990ca4e76e7364e35d711ed6e0d2a80f4
이혜성 <main@hyeseong.local>
2022-09-07 10:55:42 +0900

    4.3 create account mutation -1

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,22 @@
 import { CoreEntity } from '../../common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 
 type UserRole = 'client' | 'owner' | 'delivery';
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
     @Column()
+    @Field(typpe => String)
     email: string;
 
     @Column()
+    @Field(typpe => String)
     password: string;
 
     @Column()
+    @Field(typpe => String)
     role: UserRole;
 }
