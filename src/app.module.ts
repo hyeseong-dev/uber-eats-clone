66e4b16bc6eef17a29fab70167b1b2e0d6785584
이혜성 <main@hyeseong.local>
2022-09-19 17:36:53 +0900

    setup - 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,10 +17,10 @@ import { MailModule } from './mail/mail.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? './src/envs/.env.dev' : '.src/envs/.env.test',
+      envFilePath: process.env.NODE_ENV === 'dev' ? './src/envs/.env.dev' : './src/envs/.env.test',
       ignoreEnvFile: process.env.NODE_ENV === '.src/envs/.env.prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -42,7 +42,7 @@ import { MailModule } from './mail/mail.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
-      logging: !Boolean(process.env.DB_LOGGING),
+      logging: process.env.DB_LOGGING !== 'prod' && process.env.NODE_ENV !== 'test',
       entities: [User, Verification],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
---
+++ test.js
@@ -0,0 +1,1 @@
+function insertionSort(array) { for (let i = 1; i < array.length; i++) { let cur = array[i]; let left = i - 1; while (left >= 0 && array[left] > cur) { array[left + 1] = array[left]; left--; } array[left + 1] = cur; console.log(`${i}회전: ${array}`); } return array; } console.log(insertionSort([5, 4, 3, 2, 1]));
