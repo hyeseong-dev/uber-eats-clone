9a6a6ccc82753b26a3d222876162fead37e4873a
이혜성 <main@hyeseong.local>
2022-09-28 21:44:53 +0900

    add return type to createDish Resolver

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -101,7 +101,7 @@ export class DishResolver {
   createDish(
     @AuthUser() owner: User,
     @Args('input') createDishInput: CreateDishInput,
-  ) {
+  ): Promise<CreateDishOutput> {
     return this.restaurantService.createDish(owner, createDishInput);
   }
 
